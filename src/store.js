58429b3beaa8a112a6d23a63576acc5b0574086f
Maxime liege <liege30@gmail.com>
2018-12-03 11:18:03 +0100

    remove dev tools

--- src/store.js
+++ src/store.js
@@ -3,7 +3,6 @@ import userReducer from "./reducers/user-reducer";
 import loadingReducer from "./reducers/loading-reducer";
 import loggedReducer from "./reducers/logged-reducer";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from 'redux-devtools-extension'
 import gameReducer from './reducers/game-reducer';
 import errorReducer from './reducers/error-reducer';
 
@@ -15,12 +14,6 @@ const allReducers = combineReducers({
   error: errorReducer
 });
 
-const middleWares = [thunk];
-const middlewareEnhancer = applyMiddleware(...middleWares);
-
-const enhancers = [middlewareEnhancer];
-const composedEnhancers = composeWithDevTools(...enhancers);
-
-const store = createStore(allReducers, composedEnhancers);
+const store = createStore(allReducers, applyMiddleware(thunk));
 
 export default store;
